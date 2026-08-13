efba7b6eb0940f3f18fab3d8ca0e3412e5f50c58
Gaby <gabriela.diaz@gmail.com>
2026-08-13 19:37:08 -0300

    comment

--- src/estudiante.java
+++ src/estudiante.java
@@ -2,7 +2,8 @@ public class estudiante {
    
     String nombre;
     int nota;
-
+    
+    //cambio
     public estudiante(String nombre, int nota) {
         this.nombre = nombre;
         this.nota = nota;
